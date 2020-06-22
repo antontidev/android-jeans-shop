177d2f783fb134ef496427e5d39e381f27258ca1
pink production <sanjaplushka@gmail.com>
2020-06-22 20:15:39 +0500

    'initial'

---
+++ app/src/main/java/android/upstarts/MainActivity.kt
@@ -0,0 +1,11 @@
+package android.upstarts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/android/upstarts/di/components/New.kt
@@ -0,0 +1,4 @@
+package android.upstarts.di.components
+
+class New {
+}
